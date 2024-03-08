258da97c1292f39a73a839734d5415358bbdfdcd
Sarah <kretaksson@gmail.com>
2024-03-08 00:51:55 -0700

    bools

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -30,20 +30,29 @@
 // }
 
 
-fn main() {
-    // addition
-    let sum = 5 + 10;
+// fn main() {
+//     // addition
+//     let sum = 5 + 10;
 
-    // subtraction
-    let difference = 95.5 - 4.3;
+//     // subtraction
+//     let difference = 95.5 - 4.3;
 
-    // multiplication
-    let product = 4 * 30;
+//     // multiplication
+//     let product = 4 * 30;
 
-    // division
-    let quotient = 56.7 / 32.2;
-    let truncated = -5 / 3; // Results in -1
+//     // division
+//     let quotient = 56.7 / 32.2;
+//     let truncated = -5 / 3; // Results in -1
 
-    // remainder
-    let remainder = 43 % 5;
+//     // remainder
+//     let remainder = 43 % 5;
+// }
+
+
+fn main(){
+    let t = true;
+
+    let f: bool = false;
+
+    println!("{t}{f}")
 }
