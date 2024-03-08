8fdd8745161ec5b87546fd9f043a914e2860ed3c
Sarah <kretaksson@gmail.com>
2024-03-08 00:49:10 -0700

    floats

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -7,15 +7,24 @@
 
 
 
+// fn main() {
+//     let x = 5;
+
+//     let x = x + 1;
+
+//     {
+//         let x = x * 2;
+//         println!("The value of x in the inner scope is: {x}");
+//     }
+
+//     println!("The value of x is: {x}");
+// }
+
+
 fn main() {
-    let x = 5;
+    let x = 2.0; // f64
 
-    let x = x + 1;
+    let y: f32 = 3.0; // f32
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
-
-    println!("The value of x is: {x}");
+    println!("{x}:x, y:{y}");
 }
