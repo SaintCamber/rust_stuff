8fbae9d72934f2421489fb0ede67edf6bc5cfa41
Sarah <kretaksson@gmail.com>
2024-03-12 10:12:07 -0700

    extracted parsing logic from main fn

--- cliCommands/src/main.rs
+++ cliCommands/src/main.rs
@@ -3,14 +3,28 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    let config = Config::new(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path)
+    .expect("Should have been able to read the file");
+
+    
+
+println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+impl Config {
+fn new(args: &[String]) -> Config {
     let query = &args[1];
     let file_path = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
-
-    let contents = fs::read_to_string(file_path)
-    .expect("Should have been able to read the file");
-
-println!("With text:\n{contents}");
+    Config {query: query.to_string(),file_path: file_path.to_string()}
+}
 }
