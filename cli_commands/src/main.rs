9b0edc0ae8f55ac09a32bf0e411dffcf2dc25d34
Sarah <kretaksson@gmail.com>
2024-03-12 10:27:11 -0700

    changed file name to snake case to remove error

--- cli_commands/src/main.rs
+++ cli_commands/src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs;
+use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -21,7 +25,7 @@ struct Config {
     file_path: String,
 }
 impl Config {
-fn new(args: &[String]) -> Config {
+fn build(args: &[String]) -> Result <Config, &'static str> {
     if args.len() < 3 {
         let len = args.len();
         panic!("not enough arguments: {len} were supplied but 3 are required")
@@ -29,6 +33,6 @@ fn new(args: &[String]) -> Config {
     let query = &args[1];
     let file_path = &args[2];
 
-    Config {query: query.to_string(),file_path: file_path.to_string()}
+    Ok (Config {query: query.to_string(),file_path: file_path.to_string()})
 }
 }
