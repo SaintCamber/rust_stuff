91ba65091e4e7f94e079122a0a1c8be93e7fb8ed
Sarah <kretaksson@gmail.com>
2024-03-15 11:38:27 -0700

    finds lines that contain string now

---
+++ cli_commands/target/debug/deps/cli_commands-e6242b98324ee563.d
@@ -0,0 +1,5 @@
+/home/sarah/Rust/cli_commands/target/debug/deps/cli_commands-e6242b98324ee563: src/main.rs
+
+/home/sarah/Rust/cli_commands/target/debug/deps/cli_commands-e6242b98324ee563.d: src/main.rs
+
+src/main.rs:
---
+++ cli_commands/target/debug/deps/cli_commands-e93d02dacee59979.d
@@ -0,0 +1,5 @@
+/home/sarah/Rust/cli_commands/target/debug/deps/cli_commands-e93d02dacee59979: src/lib.rs
+
+/home/sarah/Rust/cli_commands/target/debug/deps/cli_commands-e93d02dacee59979.d: src/lib.rs
+
+src/lib.rs:
