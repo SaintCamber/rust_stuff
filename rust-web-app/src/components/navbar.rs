6b0b1d659216bc2766bcb0cda5f6116ba32ae34c
Sarah <kretaksson@gmail.com>
2024-04-04 23:49:30 -0700

    responsive dropdown for profile button is working as planned!

--- rust-web-app/src/components/navbar.rs
+++ rust-web-app/src/components/navbar.rs
@@ -1,5 +1,6 @@
 use leptos::{component, view, IntoView};
 use leptos_router::*;
+use crate::components::profileButton;
 
 #[component]
 pub fn Foot() -> impl IntoView {
@@ -15,17 +16,18 @@ pub fn Foot() -> impl IntoView {
 pub fn Nav() -> impl IntoView {
     view! {
         <header class="navbar_base">
-            <nav class="links_container">
-                <A href="/">
-                    <strong>"stays"</strong>
-                </A>
-                <A href="/">
-                    <strong>"experiences"</strong>
-                </A>
-                <A href="/">
-                <strong>"Online experiences"</strong>
-            </A>
-            </nav>
+        <nav class="links_container">
+        <A href="/">
+        <strong>"stays"</strong>
+        </A>
+        <A href="/">
+        <strong>"experiences"</strong>
+        </A>
+        <A href="/">
+        <strong>"Online experiences"</strong>
+        </A>
+        </nav>
+        <profileButton::Profile_button/>
         </header>
     }
 }
---
+++ rust-web-app/src/components/profileButton.rs
@@ -0,0 +1,34 @@
+use leptos::*;
+use leptos::html::Div;
+use leptos_use::on_click_outside;
+
+#[component]
+pub fn Profile_button() -> impl IntoView {
+    let show_menu: RwSignal<bool> = create_rw_signal(false);
+   let menu_ref = create_node_ref::<Div>();
+
+    let handle_click = move || show_menu.set(!show_menu.get());
+    let _ = on_click_outside(menu_ref, move |_| show_menu.set(false));
+    
+    view! {
+        <div node_ref=menu_ref>
+            <button on:click=move |_| { handle_click() }>profile</button>
+            <Show when=move || show_menu.get() fallback=|| ()>
+            <ul >
+                <li>
+                    <a href="#">Bookings</a>
+                </li>
+                <li>
+                    <a href="#">Your Spots</a>
+                </li>
+                <li>
+                    <a href="#">Manage Reviews</a>
+                </li>
+                <li>
+                    <a href="#">Logout</a>
+                </li>
+            </ul>
+            </Show>
+        </div>
+    }
+}
